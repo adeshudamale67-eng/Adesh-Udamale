6758fbddbfd0eac25172d1f2cbe618d2aec17695
adeshudamale67-eng <adeshudamale67@gmail.com>
2026-09-08 20:45:10 +0530

    app.py

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ if st.session_state.page == "welcome":
     with col2:
 
         if st.button(
-            "🚀Tap TO Start Prediction",
+            "🚀Tap To Start Prediction",
             use_container_width=True,
             type="primary"
         ):
